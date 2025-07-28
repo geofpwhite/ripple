9315687ef001895930d890a4cc003cf838c7df3b
Geoffrey White <geoffpiercewhite@gmail.com>
2025-07-28 18:02:57 -0400

    using updated fortio/terminal so works on linux + windows

--- ripple.go
+++ ripple.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	fpsFlag := flag.Float64("fps", 60., "change the fps")
+	fpsFlag := flag.Float64("fps", 600., "change the fps") //high fps makes it look super smooth
 	flag.Parse()
 	ap := ansipixels.NewAnsiPixels(*fpsFlag)
 	err := ap.GetSize()
@@ -17,13 +17,13 @@ func main() {
 	}
 	ap.MouseTrackingOn()
 	ap.MouseClickOn()
-	ap.GetSize()
 	defer func() {
 		ap.Restore()
 		ap.MouseTrackingOff()
 		ap.MouseClickOff()
 		ap.ShowCursor()
 		ap.MoveCursor(0, 0)
+		ap.Restore()
 	}()
 
 	err = ap.Open()
@@ -36,7 +36,6 @@ func main() {
 	ap.EndSyncMode()
 
 	for {
-		ap.MouseDecode()
 		_, err := ap.ReadOrResizeOrSignalOnce()
 		if err != nil {
 			panic("can't read/resize/signal")
@@ -44,6 +43,9 @@ func main() {
 
 		for key := range clicks {
 			clicks[key]++
+		}
+		if ap.LeftClick() {
+			clicks[[2]int{ap.Mx, ap.My}] = 0
 		}
 		Draw(ap, clicks)
 		if len(ap.Data) == 0 {
@@ -53,23 +55,15 @@ func main() {
 		case 'q':
 			ap.Restore()
 			return
-		case 32:
-			ap.Mx = int(ap.Data[1] - 32)
-			ap.My = int(ap.Data[2] - 32)
-			fmt.Println(ap.Mx, ap.My)
-			clicks[[2]int{ap.Mx, ap.My}] = 0
 		default:
 		}
-		if ap.LeftClick() {
-			fmt.Println(ap.Mx, ap.My, ap.Mbuttons)
-		}
+		fmt.Println(ap.Mx, ap.My, ap.Mbuttons)
 	}
 }
 
 func Draw(ap *ansipixels.AnsiPixels, clicks map[[2]int]int) {
 	ap.StartSyncMode()
 	ap.ClearScreen()
-	ap.GetSize()
 	for i := range ap.W {
 		for j := range ap.H {
 			for coords, radius := range clicks {
