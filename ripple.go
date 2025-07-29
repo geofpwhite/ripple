3716899f9a8a168d9f68264ed2991062a7bd9216
geofpwhite <87089088+geoffreypwhite@users.noreply.github.com>
2025-07-29 11:24:11 -0400

    colors + disk flag + HideCursor

--- ripple.go
+++ ripple.go
@@ -3,12 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand/v2"
 
 	"fortio.org/terminal/ansipixels"
 )
 
+var colorsToChoose = [...]string{
+	ansipixels.Red,
+	ansipixels.Green,
+	ansipixels.White,
+	ansipixels.Blue,
+	ansipixels.Purple,
+	ansipixels.BrightCyan,
+}
+
 func main() {
 	fpsFlag := flag.Float64("fps", 600., "change the fps") //high fps makes it look super smooth
+	filled := flag.Bool("fill", false, "makes ripples filled in")
 	flag.Parse()
 	ap := ansipixels.NewAnsiPixels(*fpsFlag)
 	err := ap.GetSize()
@@ -31,23 +42,25 @@ func main() {
 		panic("can't open")
 	}
 	clicks := make(map[[2]int]int)
+	colors := make(map[[2]int]string)
 	ap.StartSyncMode()
 	ap.ClearScreen()
 	ap.EndSyncMode()
-
+	ap.HideCursor()
 	for {
 		_, err := ap.ReadOrResizeOrSignalOnce()
 		if err != nil {
 			panic("can't read/resize/signal")
 		}
-
+		ap.MoveCursor(ap.Mx, ap.My)
 		for key := range clicks {
 			clicks[key]++
 		}
 		if ap.LeftClick() {
 			clicks[[2]int{ap.Mx, ap.My}] = 0
+			colors[[2]int{ap.Mx, ap.My}] = colorsToChoose[rand.IntN(len(colorsToChoose))]
 		}
-		Draw(ap, clicks)
+		Draw(ap, clicks, colors, *filled)
 		if len(ap.Data) == 0 {
 			continue
 		}
@@ -61,14 +74,16 @@ func main() {
 	}
 }
 
-func Draw(ap *ansipixels.AnsiPixels, clicks map[[2]int]int) {
+func Draw(ap *ansipixels.AnsiPixels, clicks map[[2]int]int, colors map[[2]int]string, filled bool) {
 	ap.StartSyncMode()
-	ap.ClearScreen()
+	if !filled {
+		ap.ClearScreen()
+	}
 	for i := range ap.W {
 		for j := range ap.H {
 			for coords, radius := range clicks {
-				if distance := (i-coords[0])*(i-coords[0]) + (j-coords[1])*(j-coords[1]); float64(distance) >= float64(radius)-(float64(radius)/20.) && float64(distance) <= float64(radius)+(float64(radius)/20.) {
-					ap.WriteAtStr(i, j, string(ansipixels.FullPixel))
+				if distance := (i-coords[0])*(i-coords[0]) + ((j*2)-(coords[1]*2))*((j*2)-(2*coords[1])); float64(distance) >= float64(radius)-(float64(radius)/20.) && float64(distance) <= float64(radius)+(float64(radius)/20.) {
+					ap.WriteAtStr(i, j, colors[coords]+string(ansipixels.FullPixel))
 				}
 			}
 			if clicks[[2]int{i, j}] >= min(ap.W, ap.H)*min(ap.W, ap.H) {
