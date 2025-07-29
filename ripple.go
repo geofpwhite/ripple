3803811555b316d6553694f796035bb51e81ff9f
Geoffrey White <geoffpiercewhite@gmail.com>
2025-07-29 16:46:54 -0400

    handle intersections
    adds randomized colors using rgb values so draw() can now take average
    of rgb values of intersecting circles and disks

--- ripple.go
+++ ripple.go
@@ -2,25 +2,58 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"math/rand/v2"
+	"strconv"
+	"strings"
 
 	"fortio.org/terminal/ansipixels"
 )
 
+const (
+	red   = "\033[38;2;250;0;0m"
+	blue  = "\033[38;2;0;0;250m"
+	green = "\033[38;2;0;250;0m"
+)
+
 var colorsToChoose = [...]string{
-	ansipixels.Red,
-	ansipixels.Green,
-	ansipixels.White,
-	ansipixels.Blue,
-	ansipixels.Purple,
-	ansipixels.BrightCyan,
+	randomColor(),
+	randomColor(),
+	randomColor(),
+	randomColor(),
+	red, blue, green,
+}
+
+func randomColor() string {
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm", rand.IntN(250), rand.IntN(250), rand.IntN(250))
+}
+
+func toRGB(s string) (int, int, int) {
+	s = s[6 : len(s)-1]
+	fields := strings.Split(s, ";")[1:]
+
+	r, err := strconv.Atoi(strings.Trim(fields[0], " "))
+	if err != nil {
+		panic(strings.Trim(fields[0], " "))
+	}
+	g, err := strconv.Atoi(strings.Trim(fields[1], " "))
+	if err != nil {
+		panic("2")
+	}
+	b, err := strconv.Atoi(strings.Trim(fields[2], " "))
+	if err != nil {
+		panic("3")
+	}
+
+	return r, g, b
 }
 
 func main() {
-	fpsFlag := flag.Float64("fps", 600., "change the fps") //high fps makes it look super smooth
+	fpsFlag := flag.Float64("fps", 600., "change the fps") // high fps makes it look super smooth
 	filled := flag.Bool("fill", false, "makes ripples filled in")
 	flag.Parse()
 	ap := ansipixels.NewAnsiPixels(*fpsFlag)
+
 	err := ap.GetSize()
 	paused := false
 	if err != nil {
@@ -81,20 +114,45 @@ func Draw(ap *ansipixels.AnsiPixels, clicks map[[2]int]int, colors map[[2]int]st
 	if !filled {
 		ap.ClearScreen()
 	}
+
 	for i := range ap.W {
 		for j := range ap.H {
+			// color := image.RGBA{}
+			r, g, b := 0, 0, 0
+			count := 0
 			for coords, radius := range clicks {
-				if distance := (i-coords[0])*(i-coords[0]) + ((j*2)-(coords[1]*2))*((j*2)-(2*coords[1])); float64(distance) >= float64(radius)-(float64(radius)/5.) && float64(distance) <= float64(radius)+(float64(radius)/5.) {
-					ap.WriteAtStr(i, j, colors[coords]+string(ansipixels.FullPixel))
+				if filled {
+					if distance := (i-coords[0])*(i-coords[0]) + ((j*2)-(coords[1]*2))*((j*2)-(2*coords[1])); float64(distance) <= float64(radius)+(float64(radius)/5.) {
+						// ap.WriteAtStr(i, j, colors[coords]+string(ansipixels.FullPixel))
+						r1, g1, b1 := toRGB(colors[coords])
+						r += r1
+						g += g1
+						b += b1
+						count++
+					}
+				} else {
+					if distance := (i-coords[0])*(i-coords[0]) + ((j*2)-(coords[1]*2))*((j*2)-(2*coords[1])); float64(distance) <= float64(radius)+(float64(radius)/5.) && float64(distance) >= float64(radius)-(float64(radius)/5.) {
+						// ap.WriteAtStr(i, j, colors[coords]+string(ansipixels.FullPixel))
+						r1, g1, b1 := toRGB(colors[coords])
+						r += r1
+						g += g1
+						b += b1
+						count++
+					}
 				}
 			}
+			if count != 0 {
+				r /= count + 1
+				g /= count + 1
+				b /= count + 1
+				ap.WriteAtStr(i, j, fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b)+string(ansipixels.FullPixel))
+			}
+
 			if clicks[[2]int{i, j}] >= min(ap.W, ap.H)*min(ap.W, ap.H) {
 				delete(clicks, [2]int{i, j})
 			}
 		}
-
 	}
 	// time.Sleep(10 * time.Millisecond)
 	ap.EndSyncMode()
-
 }
